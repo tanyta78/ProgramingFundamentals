e0ededac7ef45c9dfc82715ed97e9c3252cc2bf6
tanyta78 <tanyta78@gmail.com>
2017-07-11 18:56:51 +0300

    exams

---
+++ Exams/exam18032017/03exam18032017/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03exam18032017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine().ToLower();
+
+            if (budget <= 100)
+            {
+                Console.WriteLine("Economy class");
+                switch (season)
+                {
+                    case "summer":
+                        Console.WriteLine($"Cabrio - {budget * 0.35:f2}");
+                        break;
+                    case "winter":
+                        Console.WriteLine($"Jeep - {budget * 0.65:f2}");
+                        break;
+                }
+            }
+            else if (budget <= 500)
+            {
+                Console.WriteLine("Compact class");
+                switch (season)
+                {
+                    case "summer":
+                        Console.WriteLine($"Cabrio - {budget * 0.45:f2}");
+                        break;
+                    case "winter":
+                        Console.WriteLine($"Jeep - {budget * 0.80:f2}");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Luxury class");
+                Console.WriteLine($"Jeep - {budget * 0.90:f2}");
+               
+            }
+        }
+    }
+}
---
+++ Exams/exam18032017/exam18032017/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace exam18032017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int lastNum = int.Parse(Console.ReadLine());
+            int magicNum = int.Parse(Console.ReadLine());
+            int numberOfCombinations = 0;
+            bool isMagic = false;
+            for (int i = firstNum; i <= lastNum; i++)
+            {
+                for (int j = firstNum; j <= lastNum; j++)
+                {
+                    numberOfCombinations++;
+                    if (i + j == magicNum && !isMagic)
+                    {
+                        isMagic = true;
+                        Console.WriteLine($"Combination N:{numberOfCombinations} ({i} + {j} = {magicNum})");
+                    }
+
+                }
+            }
+
+            if (!isMagic)
+            {
+                Console.WriteLine($"{numberOfCombinations} combinations - neither equals {magicNum}");
+            }
+
+        }
+    }
+}
